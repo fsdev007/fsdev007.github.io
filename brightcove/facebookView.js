1cfc93cf769959f475e2be3030b88a9e99485b83
U-DESK-C3\Adarsh <Adarsh@desk-c3.(none)>
2014-02-09 20:29:07 +0530

    added facebook and twitter plugins

---
+++ brightcove/facebookView.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Adarsh on 1/24/14.
+ */
+(function() {
+    //console.log("************ mediaEventPlugin.js has loaded ******************");
+    var _stt_is_removed=false;
+    var _videoId;
+
+    function onPlayerReady() {
+        videoPlayer.addEventListener(brightcove.api.events.MediaEvent.BEGIN, mediaEventHandler);
+        videoPlayer.addEventListener(brightcove.api.events.MediaEvent.PLAY, mediaEventHandler);
+        var _videoId = videoPlayer.getCurrentVideo();
+    }
+
+    function mediaEventHandler(pEvent) {
+        $.get("http://dashboard.findabilitysciences.com/seventen28/api/v1/ajaxtrackvideoplayed.html?id=" + _videoId+"&source=facebook");
+        $.get("http://dashboard.findabilitysciences.com/seventen28dev/api/v1/ajaxtrackvideoplayed.html?id=" + _videoId+"&source=facebook");
+        $.get("http://dev.seventen28.com/api/v1/ajaxtrackvideoplayed.html?id=" + _videoId+"&source=facebook");
+    }
+
+    var _player = brightcove.api.getExperience();
+    var videoPlayer = _player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
+    var experience = _player.getModule(brightcove.api.modules.APIModules.EXPERIENCE);
+
+    if (experience.getReady()) {
+        onPlayerReady();
+    } else {
+        experience.addEventListener(brightcove.player.events.ExperienceEvent.TEMPLATE_READY, onPlayerReady);
+    }
+}());
